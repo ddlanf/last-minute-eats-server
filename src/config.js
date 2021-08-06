4b61f88b6ccf4ccffd87d9eba614250aa775ad64
Daniel Laufenberg <danlanfen26@gmail.com>
2021-08-05 22:17:17 -0700

    Azure Config

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     //DATABASE_URL: process.env.DATABASE_URL || 'postgresql://last_minute_chef@localhost/last_minute_eats',
+    //For Azure connection (Connection string is named as POSTGRESQLCONNSTR + {string name})
     DATABASE_URL: process.env.POSTGRESQLCONNSTR_recipedb || 'postgresql://last_minute_chef@localhost/last_minute_eats',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://last_minute_chef@localhost/last_minute_eats_test',
     CLIENT_ORIGIN: process.env.REACT_APP_BASE_URL || 'http://localhost:3000',
